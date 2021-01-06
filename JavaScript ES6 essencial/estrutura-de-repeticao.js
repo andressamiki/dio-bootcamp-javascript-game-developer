9cc1fa5615b2b1b16bf5b335a3a1239a4da961ba
Andressa Miki <andressamiki@gmail.com>
2021-01-05 22:17:40 -0300

    Estruturas de repetição

---
+++ JavaScript ES6 essencial/estrutura-de-repeticao.js
@@ -0,0 +1,35 @@
+const array = ['one', 'two', 'three'];
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(`Element #${index}: ${element}.`);
+}
+
+var n = 0;
+var x = 0;
+
+while(n<3){
+    n++;
+    x+= n;
+}
+
+console.log(x);
+
+let i = 0;
+do {
+    i += 1;
+    console.log(i);
+} while (i<5);
+
+let arr  = [3, 5, 7];
+arr.foo = "hello";
+
+for (let i in arr) {
+   console.log(i)
+}
+
+for (let i of arr) {
+    console.log(i)
+ }
+
+
